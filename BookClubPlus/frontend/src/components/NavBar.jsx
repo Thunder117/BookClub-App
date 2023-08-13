37f575d418e9d068a9722ed3544abd2f82679a3a
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2023-08-13 16:37:29 -0700

    Fix in NavBar

--- BookClubPlus/frontend/src/components/NavBar.jsx
+++ BookClubPlus/frontend/src/components/NavBar.jsx
@@ -63,9 +63,8 @@ const NavBar = (props) => {
 
                 { user && (
                     <div className="items-center h-full flex flex-row lg:gap-8">
-                        <span className={`transition ${(props.showNav && "text-black")} ${(window.innerWidth < 992) && 'hidden'}`}>{user.username}</span>
 
-                        <div className={`relative h-full w-full ${(window.innerWidth > 992) && 'hidden'}`}>
+                        <div className={`relative h-full w-full lg:hidden`}>
                             <button onClick={showUserDropdown} className="h-full w-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`h-full py-2 ${(props.showNav && "text-black")}`}>
                                     <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
@@ -79,7 +78,9 @@ const NavBar = (props) => {
                             </div>
                         </div>
 
-                        <button onClick={handleClick} className={`w-24 rounded-md border-2 border-green-700 transition duration-500 ${(props.showNav) && "text-gray-700"} ${(window.innerWidth < 992) && 'hidden'}`}>
+                        <span className={`transition ${(props.showNav && "text-black")} hidden lg:inline`}>{user.username}</span>
+                        
+                        <button onClick={handleClick} className={`w-24 rounded-md border-2 border-green-700 transition duration-500 ${(props.showNav) && "text-gray-700"} hidden lg:inline`}>
                             Log Out
                         </button>
                     </div>
