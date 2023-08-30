7912eda9c17d67e3cf25fe24b9732239e4d55f8e
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2023-08-29 23:20:19 -0700

    TODO comment in ClubTitleButton

--- BookClubPlus/frontend/src/components/ClubTitleButton.jsx
+++ BookClubPlus/frontend/src/components/ClubTitleButton.jsx
@@ -1,6 +1,7 @@
 
 const ClubTitleButton = (props) => {
 
+    // TODO: Stylize this button!
     return(
         <button className="text-start font-semibold px-4 py-2 mx-2 my-0.5 rounded-md hover:bg-gray-300">
             {props.club.title}
