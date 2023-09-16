036cc9c0e3696ccfbd6241c66f71cbc04b37593e
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2023-09-15 18:21:12 -0700

    Stylized BookTitleButton

--- BookClubPlus/frontend/src/components/BookTitleButton.jsx
+++ BookClubPlus/frontend/src/components/BookTitleButton.jsx
@@ -1,12 +1,11 @@
 const BookTitleButton = (props) => {
 
-    // TODO: Stylize this button!
     return(
         <div className={`flex`}>
 
-            <div className={`${props.bookSelected === props.book.bookId ? "border-indigo-500" : "border-blue-300"} border-l-2 flex`}></div>
+            <div className={`${props.bookSelected === props.book.bookId ? "border-indigo-500" : "border-gray-100"} border-l-2 flex`}></div>
 
-            <button onClick={() => props.bookSelector(props.book.bookId)} className="text-start font-semibold px-2 py-2 mx-2 my-0.5 rounded-md transition hover:bg-gray-300">
+            <button onClick={() => props.bookSelector(props.book.bookId)} className={`${props.bookSelected === props.book.bookId ? "bg-indigo-100" : "hover:bg-indigo-50"} text-start font-semibold px-2 py-2 mx-2 my-0.5 rounded-md transition`}>
                 {props.book.bookTitle} 
             </button>
 
