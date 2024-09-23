4d9805fb18dfaf90cd06a9ece3f8fe736e786288
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-09-23 14:34:18 -0600

    changed styling on createclubfrombookform

--- BookClubPlus/frontend/src/components/CreateClubFromBookForm.jsx
+++ BookClubPlus/frontend/src/components/CreateClubFromBookForm.jsx
@@ -56,14 +56,19 @@ const CreateClubFromBookForm = (props) => {
           
             <div className="w-full h-full sm:w-[500px] sm:h-[500px] px-6 sm:py-4 sm:rounded-md drop-shadow-2xl bg-neutral-100">
                 
-                <form className="flex flex-col h-full font-bold tracking-wide" onSubmit={handleSubmit}>
+                <form className="flex flex-col h-full gap-4 font-semibold tracking-wide" onSubmit={handleSubmit}>
 
-                    <Link to={`${props.book.key}`} className="w-24 my-2 font-bold text-xl text-center">
-                        Go back
-                    </Link>
+                    <Link to={`${props.book.key}`} className="flex gap-2 p-2 w-32 font-bold justify-center hover:bg-blue-300 rounded-full transition">
+                        <svg class="text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+                        </svg>
+                        <div>
+                            Go back
+                        </div>
+                    </Link>                
 
-                    <div className="flex justify-center py-4">
-                        <h3 className="text-3xl">Create a Club!</h3>
+                    <div className="flex justify-center">
+                        <h3 className="text-3xl font-bold">Create a Club!</h3>
                     </div>
 
                     <label>Club Name</label>
@@ -72,7 +77,7 @@ const CreateClubFromBookForm = (props) => {
                         type='text' 
                         onChange={e => setTitle(e.target.value)} 
                         value={title} maxLength="38" 
-                        className={`w-full h-10 px-2 mt-2 mb-4 rounded-sm ${emptyFields.includes('title') && 'border-2 border-red-400'}`} 
+                        className={`w-full h-12 px-4 rounded-md transition hover:border-blue-500 border-2 ${emptyFields.includes('title') && 'border-red-400'}`} 
                     />
 
                     <label>A fitting description for your club</label>
@@ -80,14 +85,16 @@ const CreateClubFromBookForm = (props) => {
                         type='text' 
                         onChange={e => setDescription(e.target.value)} 
                         value={description} 
-                        className={`w-full h-24 p-2 mt-2 mb-4 rounded-sm resize-none ${emptyFields.includes('description') && 'border-2 border-red-400'}`} 
+                        className={`w-full h-24 p-4 rounded-md resize-none transition hover:border-blue-500 border-2 ${emptyFields.includes('description') && 'border-red-400'}`} 
                     />
 
                     <div className="flex flex-row justify-end w-full">
                         {error && <div className="text-red-400 p-2 text-lg">{error}</div>}
                     </div>
-
-                    <button className="bg-sky-600 text-white rounded-sm px-6 py-4 my-4">Done!</button>
+                    
+                    <div className="flex justify-center p-4">
+                        <button className="bg-blue-500 text-white rounded-lg w-80 py-4">Done</button>
+                    </div>
                 
                 </form>
             
