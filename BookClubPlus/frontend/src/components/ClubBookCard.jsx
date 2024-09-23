6012da508cf5d0163d2e935480d4a956d0d1b457
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-09-23 14:14:09 -0600

    small styling changes

--- BookClubPlus/frontend/src/components/ClubBookCard.jsx
+++ BookClubPlus/frontend/src/components/ClubBookCard.jsx
@@ -1,17 +1,17 @@
 const ClubBookCard = (props) => {
 
     return(
-        <div className="flex justify-center items-center h-80 w-56">
+        <div className="flex justify-center items-center h-80 min-w-56">
             <button className="bg-neutral-100 flex flex-col h-72 w-52 rounded-lg ease-out duration-500 hover:h-80 hover:w-56">
                 
-                <div className="">
+                <div className="flex justify-center w-full">
                     <img 
                         alt="book_cover" 
                         src={`https://covers.openlibrary.org/b/id/${props.image}-M.jpg`} 
-                        className="w-full select-none" 
+                        className="min-h-60 max-h-60" 
                     />
                 </div>
-                <div className="">
+                <div className="w-full h-full flex items-center justify-center">
                     {props.title}
                 </div>
 
--- BookClubPlus/frontend/src/components/ClubDescription.jsx
+++ BookClubPlus/frontend/src/components/ClubDescription.jsx
@@ -19,31 +19,100 @@ const ClubDescription = (props) => {
                 </div>
 
                 <div className="flex flex-col bg-white min-h-52 max-h-52 w-[580px] p-3 m-4 rounded-xl shadow-md">
-                    <div className="flex gap-2 p-2">
 
+                    <div className="min-h-32 flex gap-4 p-2 overflow-x-auto overflow-y-hidden">
+
+                        <div className="flex flex-col h-28 min-w-24 max-w-24 items-center">
+                            <div className="flex bg-orange-500 text-white text-3xl font-semibold justify-center items-center rounded-full h-20 w-20">
+                                J
+                            </div>
+                            <div className="">
+                                Jhonny
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col h-28 min-w-24 max-w-24 items-center">
+                            <div className="flex bg-purple-500 text-white text-3xl font-semibold justify-center items-center rounded-full h-20 w-20">
+                                C
+                            </div>
+                            <div className="">
+                                Camila
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col h-28 min-w-24 max-w-24 items-center">
+                            <div className="flex bg-green-500 text-white text-3xl font-semibold justify-center items-center rounded-full h-20 w-20">
+                                E
+                            </div>
+                            <div className="">
+                                Erik
+                            </div>
+                        </div>
+
+                        
+                    </div>
+
+                    <div className="flex gap-2 p-2">
                         <button className="flex justify-center bg-blue-400 text-white min-w-16 p-3 shadow-md rounded-full text-s">
                             Add Member
                         </button>
                         <button className="flex justify-center bg-blue-400 text-white min-w-16 p-3 shadow-md rounded-full text-s">
                             Edit
                         </button>
-
                     </div>
+
                 </div>
             </div>
 
-            <div className="bg-white h-full p-2">
+            <div className="bg-white h-full p-2 overflow-x-auto">
                 
-                <div className="flex items-center justify-center gap-4 p-4 h-full">
+                <div className="flex items-center justify-start gap-4 p-4 h-full">
                     { props.club &&
                         props.club.books.map((item, index) => {
-                            console.log(item.bookTitle);
                             return(
+                            <>
                                 <ClubBookCard 
                                     id={item.bookId}
                                     title={item.bookTitle}
                                     image={item.bookImage}
                                 />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+
+                                <ClubBookCard 
+                                    id={item.bookId}
+                                    title={item.bookTitle}
+                                    image={item.bookImage}
+                                />
+                            </>          
                             )
                         })
                     }
