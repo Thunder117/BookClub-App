2940ddd038be3ccb1e6831156f82cc6eaac8f386
ChristopherHernandez117 <105824044+Thunder117@users.noreply.github.com>
2024-10-13 14:44:24 -0600

    added text to create club in the clubpage if there is no user

--- BookClubPlus/frontend/src/components/UserClubs.jsx
+++ BookClubPlus/frontend/src/components/UserClubs.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Spinner } from 'react-spinner-animated';
 import 'react-spinner-animated/dist/index.css'
+import { Link } from 'react-router-dom';
 
 // Components
 import ClubTitleButton from '../components/ClubTitleButton';
 import ClubDescription from '../components/ClubDescription';
-import { Link } from 'react-router-dom';
 
 const UserClubs = (props) => {
     const [clubs, setClubs] = useState(0); 
@@ -44,64 +44,59 @@ const UserClubs = (props) => {
     return(
         <div className="flex flex-col lg:flex-row w-full pt-20 gap-4 lg:gap-0"> {/* All Columns */}
 
-            { clubs.length > 0  && 
-            <>
+        { clubs.length > 0  && 
+        <>
 
-                {/* Left Column */}
-                <div className="w-full lg:w-1/6 max-h-full">
-                    <div className="flex lg:flex-col gap-3 h-full overflow-y-auto bg-gradient-to-br from-blue-500 to-blue-600 rounded-xl py-10"> 
-
-                        { clubs.map((club, index) => {
-                                
-                            return <ClubTitleButton club={club} clubSelected={clubSelected} clubSelector={clubSelector} key={index}/>
-                                
-                        })}
-
-                    </div>
-                </div>
-
-                {/* Right Column */}
-                <div className="flex w-full lg:w-5/6 justify-center"> 
+            {/* Left Column */}
+            <div className="w-full lg:w-1/6 max-h-full">
+                <div className="flex lg:flex-col gap-3 h-full overflow-y-auto bg-gradient-to-br from-blue-500 to-blue-600 rounded-xl py-10"> 
 
                     { clubs.map((club, index) => {
-                        
-                        if(clubSelected === club._id) {
-                            return(
-                                <ClubDescription 
-                                    key={index}    
-                                    club={club} 
-                                    clubs={clubs}  // Pass the clubs array
-                                    setClubs={setClubs}  // Pass the setter function
-                                    token={props.user.token}    
-                                />
-                            )
-                        }
-                        return null;
+                            
+                        return <ClubTitleButton club={club} clubSelected={clubSelected} clubSelector={clubSelector} key={index}/>
                             
                     })}
 
                 </div>
+            </div>
+
+            {/* Right Column */}
+            <div className="flex w-full lg:w-5/6 justify-center"> 
+                { clubs.map((club, index) => {
+                    if(clubSelected === club._id) {
+                        return(
+                            <ClubDescription 
+                                key={index}    
+                                club={club} 
+                                clubs={clubs}  // Pass the clubs array
+                                setClubs={setClubs}  // Pass the setter function
+                                token={props.user.token}    
+                            />
+                        )
+                    }
+                    return null;
+                })}
+            </div>
                     
-            </>
-            }
+        </>
+        }
             
-            {clubs.length === 0  && 
-                <div className="flex flex-col gap-8 w-full h-4/6 justify-center items-center">
-                    <div className="text-xl font-semibold">Oops... It looks like you are not in a club yet</div>
-                    <Link to={'/'} className="bg-blue-500 text-white font-semibold px-4 py-2 text-center rounded-full">Create a Club</Link>
-                </div>
-            }
+        {clubs.length === 0  && 
+            <div className="flex flex-col gap-8 w-full h-4/6 justify-center items-center">
+                <div className="text-xl font-semibold">Oops... It looks like you are not in a club yet</div>
+                <Link to={'/'} className="bg-blue-500 text-white font-semibold px-4 py-2 text-center rounded-full">Create a Club</Link>
+            </div>
+        }
 
-            { (isLoadingClubs && clubs.length !== 0) &&
-                <div className="flex w-full h-4/6 justify-center items-center">
-                    <Spinner  
-                        center={false} 
-                        width={"150px"} 
-                        height={"150px"}
-                    />
-                </div>
-            }
-
+        { (isLoadingClubs && clubs.length !== 0) &&
+            <div className="flex w-full h-4/6 justify-center items-center">
+                <Spinner  
+                    center={false} 
+                    width={"150px"} 
+                    height={"150px"}
+                />
+            </div>
+        }
         </div>
     );
 };
--- BookClubPlus/frontend/src/pages/ClubsPage.jsx
+++ BookClubPlus/frontend/src/pages/ClubsPage.jsx
@@ -1,4 +1,5 @@
 import { useAuthContext } from '../hooks/useAuthContext';
+import { Link } from 'react-router-dom';
 
 // Components
 import NavBar from '../components/NavBar';
@@ -13,9 +14,15 @@ const ClubsPage = () => {
 
             <div className="font-sans bg-neutral-100 flex h-full"> {/* ALL */}
 
-                { user && 
-                    <UserClubs user={user} />
-                }
+            { user 
+            ?
+                <UserClubs user={user} />
+            :
+            <div className="flex flex-col gap-8 w-full h-4/6 justify-center items-center">
+                <div className="text-xl font-semibold">Oops... It looks like you are not in a club yet</div>
+                <Link to={'/'} className="bg-blue-500 text-white font-semibold px-4 py-2 text-center rounded-full">Create a Club</Link>
+            </div>
+            }
 
             </div>
         </div>
